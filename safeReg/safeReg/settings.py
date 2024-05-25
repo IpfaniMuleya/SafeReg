5b9d34f7492591843b391ef0f7e75ddb666984df
Ipfani Muleya <muleyaipfani22@gmail.com>
2024-05-26 01:23:14 +0200

    take 2

--- safeReg/safeReg/settings.py
+++ safeReg/safeReg/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = env('THE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #remeber to change to False
 
-ALLOWED_HOSTS = [] # use * or use proper domain name
+ALLOWED_HOSTS = [
+    'localhost',  
+    'safereg.onrender.com',  
+]
 
 
 # Application definition
@@ -167,6 +170,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'SafeReg/safeReg/static'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
